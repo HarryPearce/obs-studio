05c77d8966b1b040ad90abe10cbefa86dd98539d
HarryPearce <harrypearcegaming@gmail.com>
2019-05-04 16:07:16 +0200

    hw accel

--- plugins/vlc-video/vlc-video-source.c
+++ plugins/vlc-video/vlc-video-source.c
@@ -437,6 +437,8 @@ static void add_file(struct vlc_source *c, struct darray *array,
 			dstr_free(&network_caching_option);
 		}
 
+		libvlc_media_add_option_(new_media, ":avcodec-hw=any");
+
 		data.path = new_path.array;
 		data.media = new_media;
 		da_push_back(new_files, &data);
